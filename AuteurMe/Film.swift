9d1a3fb2e67449e102c93b9820feec33ebc998d7
Rakhi <rakhibhardwaj@rakhis-macbook-pro.local>
2018-12-14 01:27:40 +0530

    2.Json Parsing

---
+++ AuteurMe/Film.swift
@@ -0,0 +1,16 @@
+//
+//  Film.swift
+//  AuteurMe
+//
+//  Created by Rakhi on 14/12/18.
+//  Copyright © 2018 Rakhi. All rights reserved.
+//
+
+import Foundation
+
+struct Film : Codable {
+    let title: String
+    let year: String
+    let poster: String
+    let plot: String
+}
--- AuteurMe/ViewController.swift
+++ AuteurMe/ViewController.swift
@@ -9,10 +9,60 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let auteurs = Auteur.auteursFromBundle()
+    @IBOutlet var tblv: UITableView!
+    
+    // MARK:-  VC LIFE CYCLE
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
+    
+    
+    // MARK:-  INIT
+    func initFunction()  {
+        
+    }
+    
+    // MARK:-  SET UI
+    func setUI()  {
+        let myAttribute = [NSAttributedStringKey.foregroundColor: UIColor.blue,NSAttributedStringKey.font:UIFont.boldSystemFont(ofSize: 22)]
+        if #available(iOS 11.0, *) {
+            navigationController?.navigationBar.largeTitleTextAttributes = myAttribute
+            navigationItem.largeTitleDisplayMode = .automatic
+        } else {
+            navigationController?.navigationBar.titleTextAttributes = myAttribute
+        }
+        
+    }
+    
 }
 
+
+extension ViewController : UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return auteurs.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let auteur = auteurs[indexPath.row]
+        cell.textLabel?.text = auteur.bio
+        return cell
+    }
+}
+
+
+
+
+
+
+
+
+
+
+/*
+let myAttribute = [NSAttributedStringKey.foregroundColor: UIColor.blue,NSAttributedStringKey.font:UIFont.boldSystemFont(ofSize: 22)]
+navigationController?.navigationBar.largeTitleTextAttributes = myAttribute
+navigationItem.largeTitleDisplayMode = .automatic
+*/
